addd54a70cfc665989c03d0f649712c235aaa7b5
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-22 15:40:57 -0600

    add getSadInterval function

---
+++ mole.js
@@ -0,0 +1,12 @@
+function getSadInterval() {
+  return Date.now() + 500;
+}
+
+const moles = [
+  {
+    status: "sad",
+    next: Date.now() + 1000,
+    king: false,
+    node: document.querySelector("#hole-0"),
+  },
+];
